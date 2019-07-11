1765b6f764ad9079b61875900a79dec16c58f34e
Danusia-Sales <49010337+Danusia-Sales@users.noreply.github.com>
2019-07-11 10:23:59 -0300

    Código da lista de endereço do cliente

--- wish/Cliente.java
+++ wish/Cliente.java
@@ -1,12 +1,35 @@
 package wish;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Cliente {
 
     String nome;
+    int CPF;
     int CEP;
+    int telefone;
     Cliente endereço;
 
     public Cliente(String n) {
         this.nome = n;
+    }
+}
+
+public class Lista {
+
+    public static void main(String[] args) {
+        Collection<String> endereco = new ArrayList();
+        endereco.add("Carapebus");
+        endereco.add("Macaé");
+        endereco.add("Rio das Ostras");
+        endereco.add("Campos dos Goytacazes");
+        endereco.add("Arraial do Cabo");
+        endereco.add("Bruxelas");
+        endereco.add("Camarões");
+        endereco.add("Cabo Frio");
+
+        System.out.println("Lista de endereço: " + endereco);
+
     }
 }
